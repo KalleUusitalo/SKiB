bb3aba2768ebe570e69cf4ae024e0b799634f265
KalleUusitalo <kalle.uusitalo@mvr-yhtyma.fi>
2025-04-28 09:31:36 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   apiKey: "AIzaSyD3N1jRcAwSBVzEhMLfyXG37StORE4Qr4U",
   authDomain: "skib-d8f58.firebaseapp.com",
-  databaseURL: "https://skib-d8f58-default-rtdb.europe-west1.firebasedatabase.app",
+  databaseURL: "https://skib-d8f58-default-rtdb.europe-west1-firebasedatabase.app",
   projectId: "skib-d8f58",
   storageBucket: "skib-d8f58.firebasestorage.app",
   messagingSenderId: "550856600406",
