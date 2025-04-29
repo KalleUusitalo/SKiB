24514f63b4c42f5c3cc23c1435110686b212daba
mvr-skib <kalle.uusitalo@mvr-yhtyma.fi>
2025-04-29 21:23:58 +0300

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyD3N1jRcAwSBVzEhMLfyXG37StORE4Qr4U",
-  authDomain: "skib-d8f58.firebaseapp.com",
-  databaseURL: "https://skib-d8f58-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "skib-d8f58",
-  storageBucket: "skib-d8f58.firebasestorage.app",
-  messagingSenderId: "550856600406",
-  appId: "1:550856600406:web:c8222181b81580c95684f7"
-};
